93e42a875c216aa740ed5a432640881a75cff5ca
JakubOpar <oparkuba2002@gmail.com>
2024-05-14 20:00:02 +0200

    Laczeie bootstrapa z laravelem cd.

--- szpital/routes/web.php
+++ szpital/routes/web.php
@@ -10,3 +10,27 @@ Route::get('/zaloguj', function () {
     return view('Logowanie');
 });
 
+Route::get('/lekarzeTab', function () {
+    return view('lekarzeTab');
+});
+
+Route::get('/pielegniarkiTab', function () {
+    return view('pielegniarkiTab');
+});
+
+Route::get('/pacjenciTab', function () {
+    return view('pacjenciTab');
+});
+
+Route::get('/lekiTab', function () {
+    return view('lekiTab');
+});
+
+Route::get('/saleTab', function () {
+    return view('saleTab');
+});
+
+Route::get('/admin', function() {
+    return view('admin');
+}); //admin autoryzacja
+
