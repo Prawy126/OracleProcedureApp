655f5e9317474ae4e889751b95a7b338b90c38aa
Prawy_126 <michal190713@gmail.com>
2024-05-18 21:23:51 +0200

    Aktualizajca procedur

--- szpital/routes/web.php
+++ szpital/routes/web.php
@@ -29,9 +29,7 @@ Route::get('/pacjenciTab', function () {
     return view('pacjenciTab');
 })->name('pacjenciTab');
 
-Route::get('/saleTab', function () {
-    return view('saleTab');
-})->name('saleTab');
+
 
 Route::get('/admin', function() {
     return view('admin');
