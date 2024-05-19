d28863042398cc449cb09a0641f9a555850ee1ea
JakubOpar <oparkuba2002@gmail.com>
2024-05-19 19:13:42 +0200

    Prace nad CRUD cd

--- szpital/app/Http/Controllers/DoctorController.php
+++ szpital/app/Http/Controllers/DoctorController.php
@@ -18,20 +18,6 @@ class DoctorController extends Controller
     public function store(Request $request)
     {
         DB::transaction(function () use ($request) {
-            DB::statement('
-                CREATE OR REPLACE PROCEDURE ADD_DOCTOR(
-                    p_name IN VARCHAR2,
-                    p_surname IN VARCHAR2,
-                    p_specialization IN VARCHAR2,
-                    p_license_number IN VARCHAR2,
-                    p_user_id IN NUMBER)
-                IS
-                BEGIN
-                    INSERT INTO doctors (name, surname, specialization, license_number, user_id)
-                    VALUES (p_name, p_surname, p_specialization, p_license_number, p_user_id);
-                END;'
-            );
-
             DB::statement('BEGIN ADD_DOCTOR(:name, :surname, :specialization, :license_number, :user_id); END;', [
                 'name' => $request->input('name'),
                 'surname' => $request->input('surname'),
@@ -46,21 +32,26 @@ class DoctorController extends Controller
 
     public function edit($id)
     {
-        $zap = 'CREATE OR REPLACE PROCEDURE GET_DOCTOR(
-            p_doctor_id IN NUMBER,
-            p_doctor OUT SYS_REFCURSOR)
-        IS
-        BEGIN
-            OPEN p_doctor FOR
-            SELECT * FROM doctors WHERE id = p_doctor_id;
-        END;';
+        // Wywołanie istniejącej procedury składowanej
+        $pdo = DB::getPdo();
+        $stmt = $pdo->prepare("BEGIN GET_DOCTOR(:id, :doctor); END;");
 
-        DB::statement($zap);
+        // Rejestracja zmiennych wejściowych i wyjściowych
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':doctor', $doctorCursor, PDO::PARAM_STMT);
 
-        $doctor = DB::select('BEGIN GET_DOCTOR(:id, :doctor); END;', [
-            'id' => $id,
-            'doctor' => null
-        ]);
+        // Wykonanie procedury
+        $stmt->execute();
+
+        // Fetchowanie danych z kursora
+        oci_execute($doctorCursor, OCI_DEFAULT);
+        $doctor = [];
+        while ($row = oci_fetch_assoc($doctorCursor)) {
+            $doctor[] = $row;
+        }
+
+        // Zamknięcie kursora
+        oci_free_statement($doctorCursor);
 
         return view('edycjaLekarze', compact('doctor'));
     }
@@ -68,26 +59,7 @@ class DoctorController extends Controller
     public function update(Request $request, $id)
     {
         DB::transaction(function() use ($request, $id) {
-            DB::statement('
-                CREATE OR REPLACE PROCEDURE UPDATE_DOCTOR(
-                    p_doctor_id IN NUMBER,
-                    p_name IN VARCHAR2,
-                    p_surname IN VARCHAR2,
-                    p_specialization IN VARCHAR2,
-                    p_license_number IN VARCHAR2,
-                    p_user_id IN NUMBER)
-                IS
-                BEGIN
-                    UPDATE doctors
-                    SET name = p_name,
-                        surname = p_surname,
-                        specialization = p_specialization,
-                        license_number = p_license_number,
-                        user_id = p_user_id
-                    WHERE id = p_doctor_id;
-                END;'
-            );
-
+            // Wywołanie istniejącej procedury składowanej
             DB::statement('BEGIN UPDATE_DOCTOR(:id, :name, :surname, :specialization, :license_number, :user_id); END;', [
                 'id' => $id,
                 'name' => $request->input('name'),
@@ -105,15 +77,7 @@ class DoctorController extends Controller
     {
         try {
             DB::transaction(function() use ($id) {
-                DB::statement('
-                    CREATE OR REPLACE PROCEDURE DELETE_DOCTOR(
-                        p_doctor_id IN NUMBER)
-                    IS
-                    BEGIN
-                        DELETE FROM doctors WHERE id = p_doctor_id;
-                    END;'
-                );
-
+                // Wywołanie istniejącej procedury składowanej
                 DB::statement('BEGIN DELETE_DOCTOR(:id); END;', [
                     'id' => $id
                 ]);
